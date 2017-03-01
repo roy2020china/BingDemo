630536fbb62deb97283ba59f3f8d1e11aadfaa1d
zhang bing <40224272@qq.com>
2017-03-01 09:55:44 +0800

    Update 8_number_of_strings_starting_with_U.py

--- 8_number_of_strings_starting_with_U.py
+++ 8_number_of_strings_starting_with_U.py
@@ -1,6 +1,8 @@
 # Given a list of strings, define a procedure to return the count of strings starting with a upper case letter "U"
 # 给定某一字符串列表，写一个函数，返回以大写字母U开头的字符串的个数
-def measure_udacity(list_of_strs):
+
+# version 1 版本1
+def measure_u(list_of_strs):
     search_results = []
     for each in list_of_strs:
         search_for = "U"
@@ -14,3 +16,12 @@ def start_U_or_not(search_for, string):
         return True
     else:
         return False
+
+    
+# version 2 版本2
+def measure_U(list_of_strs):
+    count = 0
+    for each in list_of_strs:
+        if each[0] == "U":
+            count += 1
+    return count
